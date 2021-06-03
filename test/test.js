d993d75835b50fcd866b136ff17e093bab25a873
Xiao Tan <tvytlx@gmail.com>
2021-06-03 15:26:42 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,27 +1,29 @@
-import HTMLParser from "../src/html-parser";
+import htmlParsing from "../src/html-parser";
 
 var assert = require("assert");
 describe("html parser", function () {
-  it("raw html show return correct", function () {
-    const parser = new HTMLParser();
-    console.log(
-      parser.HTMLParser(
-        `<html><body><div :attr="132">hello</div></body></html>`,
-        {
-          start: function (tag, attrs, unary) {
-            console.log("start", arguments);
-          },
-          end: function (tag) {
-            console.log("end", arguments);
-          },
-          chars: function (text) {
-            console.log("chars", arguments);
-          },
-          comment: function (text) {
-            console.log("comment", arguments);
-          },
-        }
-      )
+  it("html string parsing should return correct result", function () {
+    const result = ['html', 'body', 'div', 'hello', 'div', 'body', 'html'];
+    let index = 0;
+    console.log(typeof htmlParsing)
+    htmlParsing(
+      `<html><body><div :attr="132" @attr2="1333">hello</div></body></html>`,
+      {
+        start: function (tag, attrs, unary) {
+          assert.strictEqual(result[index], tag);
+          index += 1;
+        },
+        end: function (tag) {
+          assert.strictEqual(result[index], tag)
+          index += 1;
+        },
+        chars: function (text) {
+          assert.strictEqual(result[index], text)
+          index += 1;
+        },
+        comment: function (text) {
+        },
+      }
     );
-  });
+  })
 });
